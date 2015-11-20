e7fe5381397a354356260e9652e7055898065dc3
Stan Callewaert <callewaert.stan@gmail.com>
2015-11-20 18:48:35 +0100

    Small HTML changes

--- themes/appsforghent/assets/javascript/header.js
+++ themes/appsforghent/assets/javascript/header.js
@@ -4,11 +4,11 @@ $(window).load(function() {
 
 var header = function() {   
     if($(document).scrollTop() > 0) {
-        $('nav').css({'position': 'fixed', 'background-color': 'white'});
+        $('nav').css({'background-color': 'white'});
         $('.nav').removeClass('nav').addClass('nav-low');
         $('#left a').html('<img src="themes/appsforghent/assets/images/logo-black.png" alt="Logo Open Belgium 2016" />')
     } else {
-        $('nav').css({'position': 'relative', 'background-color': 'transparent'});
+        $('nav').css({'background-color': 'transparent'});
         $('.nav-low').removeClass('nav-low').addClass('nav');
         $('#left a').html('<img src="themes/appsforghent/assets/images/logo.png" alt="Logo Open Belgium 2016" />')
     }
