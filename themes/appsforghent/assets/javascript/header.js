ef145e6cd077a175ff1a139cfeac05a60dd63462
Stan Callewaert <callewaert.stan@gmail.com>
2015-11-22 19:36:57 +0100

    Added a responsive menu + other small changes

--- themes/appsforghent/assets/javascript/header.js
+++ themes/appsforghent/assets/javascript/header.js
@@ -2,14 +2,16 @@ $(window).load(function() {
     $(window).scroll(header);
 });
 
-var header = function() {   
-    if($(document).scrollTop() > 0) {
-        $('nav').css({'background-color': 'white'});
-        $('.nav').removeClass('nav').addClass('nav-low');
-        $('#left a').html('<img src="themes/appsforghent/assets/images/logo-black.png" alt="Logo Open Belgium 2016" />')
-    } else {
-        $('nav').css({'background-color': 'transparent'});
-        $('.nav-low').removeClass('nav-low').addClass('nav');
-        $('#left a').html('<img src="themes/appsforghent/assets/images/logo.png" alt="Logo Open Belgium 2016" />')
+var header = function() {
+    if($(window).width() >= 900) { 
+        if($(document).scrollTop() > 0) {
+            $('nav').css({'background-color': 'white'});
+            $('.nav').removeClass('nav').addClass('nav-low');
+            $('#left a').html('<img src="themes/appsforghent/assets/images/logo-black.png" alt="Logo Open Belgium 2016" />');
+        } else {
+            $('nav').css({'background-color': 'transparent'});
+            $('.nav-low').removeClass('nav-low').addClass('nav');
+            $('#left a').html('<img src="themes/appsforghent/assets/images/logo.png" alt="Logo Open Belgium 2016" />');
+        }
     }
 }
--- themes/appsforghent/assets/javascript/headerHome.js
+++ themes/appsforghent/assets/javascript/headerHome.js
@@ -2,12 +2,14 @@ $(window).load(function() {
     $(window).scroll(header);
 });
 
-var header = function() {   
-    if($(document).scrollTop() >= $(window).height()) {
-        $('nav').css({'position': 'fixed', 'background-color': 'white'});
-        $('.nav').removeClass('nav').addClass('nav-low');
-    } else {
-        $('nav').css({'position': 'relative', 'background-color': 'transparent'});
-        $('.nav-low').removeClass('nav-low').addClass('nav');
+var header = function() {
+    if($(window).width() >= 900) {
+        if($(document).scrollTop() >= $(window).height()) {
+            $('nav').css({'position': 'fixed', 'background-color': 'white'});
+            $('.nav').removeClass('nav').addClass('nav-low');
+        } else {
+            $('nav').css({'position': 'relative', 'background-color': 'transparent'});
+            $('.nav-low').removeClass('nav-low').addClass('nav');
+        }
     }
 }
