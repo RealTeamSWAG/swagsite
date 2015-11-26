34c6eb1342fe22765b81a3773c8210f1ed2dec24
Stan Callewaert <callewaert.stan@gmail.com>
2015-11-26 15:09:49 +0100

    Dynamic programme, adapt to values in CMS

--- plugins/teamswag/appsforx/components/Sessions.php
+++ plugins/teamswag/appsforx/components/Sessions.php
@@ -24,15 +24,20 @@ class Sessions extends ComponentBase
     public function onRun()
     {
         $this->sessions = Session::all()->toArray();
+        $this->locationsBuilder;
 
-        $this->locationsBuilder = "";
+        $locations = Session::all()->sortBy('location');
 
-        for($i = 0; $i < count($this->sessions); $i++) {
+        for($i = 0; $i < count($locations); $i++) {
             if($i == 0) {
-                $this->locationsBuilder += "'" + $this->sessions[$i]['location'] + "'";
+                $this->locationsBuilder .= "'" . $locations[$i]['location'] . "'";
             } else {
-                $this->locationsBuilder += ", '" + $this->sessions[$i]['location'] + "'";
+                $this->locationsBuilder .= ", '" . $locations[$i]['location'] . "'";
             }
+        }
+
+        foreach($this->sessions as &$session) {
+            $session['end_time'] = gmdate("Y-m-d H:i:s", strtotime($session['start_time']) + ($session['duration'] * 60));
         }
     }
 }
