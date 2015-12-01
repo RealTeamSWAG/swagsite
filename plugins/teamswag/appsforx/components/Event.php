43dd5648071002ae436a66f622c2f33e7c4001a7
Stan Callewaert <callewaert.stan@gmail.com>
2015-12-01 15:51:30 +0100

    Event page indicate what a specific value is

--- plugins/teamswag/appsforx/components/Event.php
+++ plugins/teamswag/appsforx/components/Event.php
@@ -27,22 +27,5 @@ class Event extends ComponentBase
         $this->event = Session::where('id', $pid)->first()->load('speakers');
         $this->event['start'] = gmdate("H\hi", strtotime($this->event['start_time']));
         $this->event['end'] = gmdate("H\hi", strtotime($this->event['start_time']) + ($this->event['duration'] * 60));
-
-        /*$this->sessions = Session::all()->toArray();
-        $this->locationsBuilder;
-
-        $locations = Session::all()->sortBy('location');
-
-        for($i = 0; $i < count($locations); $i++) {
-            if($i == 0) {
-                $this->locationsBuilder .= "'" . $locations[$i]['location'] . "'";
-            } else {
-                $this->locationsBuilder .= ", '" . $locations[$i]['location'] . "'";
-            }
-        }
-
-        foreach($this->sessions as &$session) {
-            $session['end_time'] = gmdate("Y-m-d H:i:s", strtotime($session['start_time']) + ($session['duration'] * 60));
-        }*/
     }
 }
