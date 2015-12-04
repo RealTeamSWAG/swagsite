03b560426ff4449b988e84bc6b88256c527e04c1
Stan Callewaert <callewaert.stan@gmail.com>
2015-12-04 12:33:30 +0100

    Nicer url event page, accentjes en andere speciale tekens kunnen nu wel in de timetable

--- plugins/teamswag/appsforx/components/Event.php
+++ plugins/teamswag/appsforx/components/Event.php
@@ -17,13 +17,15 @@ class Event extends ComponentBase
 
     public function defineProperties()
     {
-        return [];
+        return [
+        ];
     }
 
 
     public function onRun()
     {
-        $pid = $_GET["id"]; 
+        $pid = $this->param('id');
+        //$pid = $_GET["id"]; 
         $this->event = Session::where('id', $pid)->first()->load('speakers');
         $this->event['start'] = gmdate("H\hi", strtotime($this->event['start_time']));
         $this->event['end'] = gmdate("H\hi", strtotime($this->event['start_time']) + ($this->event['duration'] * 60));
